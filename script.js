f7bec5691db0ee2edec63ffdc157c053b0246cf0
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-13 17:31:37 -0300

    Concluindo o 4º requisito e melhorando o 2º requisito

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (intMax - intMin)) + intMin;
 }
 
-function rgbRandomValueGenerator() {
-  const red = getRandomInt(0, 256);
-  const green = getRandomInt(0, 256);
-  const blue = getRandomInt(0, 256);
+function defineRandomBackgroundColorBalls() {
+  const balls = document.querySelectorAll('.ball');
+  for (let index = 0; index < balls.length; index += 1) {
+    const red = getRandomInt(0, 256);
+    const green = getRandomInt(0, 256);
+    const blue = getRandomInt(0, 256);
+    balls[index].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  }
+}
+
+function showBackgroundColorRandomBall() {
+  const randomIndex = getRandomInt(0, 6);
+  const balls = document.querySelectorAll('.ball');
   const divRgbColor = document.getElementById('rgb-color');
-  divRgbColor.innerText = `(${red}, ${green}, ${blue})`;
+  const rgb = balls[randomIndex].style.backgroundColor.slice(3);
+  divRgbColor.innerText = `${rgb}`;
 }
 
 window.onload = () => {
-  rgbRandomValueGenerator();
+  defineRandomBackgroundColorBalls();
+  showBackgroundColorRandomBall();
 };
